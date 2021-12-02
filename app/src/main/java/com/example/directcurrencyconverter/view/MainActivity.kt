7f3cdbee573c920516432d90361f1bb898d92e6b
Gilbert Ngeno <ngenogilbert07@gmail.com>
2021-12-02 11:15:19 +0300

    BUILT VIEW/LAYOUT

--- app/src/main/java/com/example/directcurrencyconverter/view/MainActivity.kt
+++ app/src/main/java/com/example/directcurrencyconverter/view/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import com.example.directcurrencyconverter.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.AppTheme_NoActionBar);
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
