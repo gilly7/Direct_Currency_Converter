afb08a47f2f6371c6d7b38a268cdf4c92a5fad02
Gilbert Ngeno <ngenogilbert07@gmail.com>
2021-12-02 09:59:29 +0300

    Added dependencies and permissions on manifest.xml

---
+++ app/src/main/java/com/example/directcurrencyconverter/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.directcurrencyconverter.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.directcurrencyconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
